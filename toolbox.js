35cb18af937932c025c4b80ce2331f66ca217b35
saundersp <pierre.saundersgb@gmail.com>
2024-07-18 00:05:57 +0200

    Fixed cleanup function

--- toolbox.js
+++ toolbox.js
@@ -162,7 +162,7 @@
 
 	exports.getCurrentDateTime = _ => {
 		const c = new Date();
-		const n = n => (n < 10 ? "0" : "") + n;
+		const n = n => (n < 10 ? '0' : '') + n;
 		const d = n(c.getDate());
 		const m = n(c.getMonth() + 1);
 		const y = c.getFullYear();
@@ -322,7 +322,7 @@
 		}
 
 		function rhex(n) {
-			const hex_chr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+			const hex_chr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 			let s = '';
 			for (let j = 0; j < 4; j++)
 				s += hex_chr[(n >> (j * 8 + 4)) & 0x0F] +
@@ -343,29 +343,29 @@
 	};
 
 	exports.ask = opt => {
-		let query = opt.link ? opt.link + "?" : "/?";
-		let responseType = "text",
-			requestType = "GET";
+		let query = opt.link ? opt.link + '?' : '/?';
+		let responseType = 'text',
+			requestType = 'GET';
 		if (opt.name) {
 			query += opt.name;
-			if (opt.value) query += "=" + opt.value;
+			if (opt.value) query += '=' + opt.value;
 		}
 		if (opt.responseType) {
 			if ([
-					"text",
-					"arraybuffer",
-					"blob",
-					"document",
-					"json",
-					"ms-stream",
-					"moz-chunked-arraybuffer"
+					'text',
+					'arraybuffer',
+					'blob',
+					'document',
+					'json',
+					'ms-stream',
+					'moz-chunked-arraybuffer'
 				].indexOf(opt.responseType) === -1)
-				throw "Invalid responseType";
+				throw 'Invalid responseType';
 			else responseType = opt.responseType;
 		}
 		if (opt.requestType) {
-			if (["GET", "POST", "PUT", "DELETE"].indexOf(opt.requestType.toUpperCase()) === -1)
-				throw "Invalid requestType";
+			if (['GET', 'POST', 'PUT', 'DELETE'].indexOf(opt.requestType.toUpperCase()) === -1)
+				throw 'Invalid requestType';
 			else requestType = opt.requestType;
 		}
 		return new Promise((resolve, reject) => {
@@ -375,7 +375,7 @@
 			x.onreadystatechange = () => {
 				if (x.readyState === 4) {
 					if (x.status === 200) resolve(x.response);
-					else reject("Request failed : " + x.statusText);
+					else reject('Request failed : ' + x.statusText);
 				}
 			};
 			x.open(requestType, query, true);
@@ -392,72 +392,72 @@
 			dom.html = value => dom.forEach(obj => (obj.innerHTML = value));
 			dom.show = _ => {
 				const displayValues = {
-					address: "block",
-					area: "block",
-					article: "block",
-					aside: "block",
-					blockquote: "block",
-					body: "block",
-					caption: "table-caption",
-					col: "table-column",
-					colgroup: "",
-					datalist: "block",
-					dd: "block",
-					details: "block",
-					div: "block",
-					dl: "block",
-					dt: "block",
-					fieldset: "block",
-					figcaption: "block",
-					figure: "block",
-					footer: "block",
-					form: "block",
-					h1: "block",
-					h2: "block",
-					h3: "block",
-					h4: "block",
-					h5: "block",
-					h6: "block",
-					head: "block",
-					header: "block",
-					hr: "block",
-					html: "block",
-					img: "inline-block",
-					legend: "block",
-					li: "list-item",
-					link: "block",
-					map: "inline",
-					menu: "block",
-					nav: "block",
-					ol: "block",
-					output: "inline",
-					p: "block",
-					param: "block",
-					pre: "block",
-					q: "inline",
-					script: "block",
-					section: "block",
-					style: "block",
-					summary: "block",
-					table: "table",
-					tbody: "table-row-group",
-					td: "table-cell",
-					tfoot: "table-footer-group",
-					th: "table-cell",
-					thead: "table-header-group",
-					title: "block",
-					tr: "table-row",
-					ul: "block"
+					address: 'block',
+					area: 'block',
+					article: 'block',
+					aside: 'block',
+					blockquote: 'block',
+					body: 'block',
+					caption: 'table-caption',
+					col: 'table-column',
+					colgroup: '',
+					datalist: 'block',
+					dd: 'block',
+					details: 'block',
+					div: 'block',
+					dl: 'block',
+					dt: 'block',
+					fieldset: 'block',
+					figcaption: 'block',
+					figure: 'block',
+					footer: 'block',
+					form: 'block',
+					h1: 'block',
+					h2: 'block',
+					h3: 'block',
+					h4: 'block',
+					h5: 'block',
+					h6: 'block',
+					head: 'block',
+					header: 'block',
+					hr: 'block',
+					html: 'block',
+					img: 'inline-block',
+					legend: 'block',
+					li: 'list-item',
+					link: 'block',
+					map: 'inline',
+					menu: 'block',
+					nav: 'block',
+					ol: 'block',
+					output: 'inline',
+					p: 'block',
+					param: 'block',
+					pre: 'block',
+					q: 'inline',
+					script: 'block',
+					section: 'block',
+					style: 'block',
+					summary: 'block',
+					table: 'table',
+					tbody: 'table-row-group',
+					td: 'table-cell',
+					tfoot: 'table-footer-group',
+					th: 'table-cell',
+					thead: 'table-header-group',
+					title: 'block',
+					tr: 'table-row',
+					ul: 'block'
 				};
 				dom.forEach(obj => {
 					const nodename = obj.nodeName.toLowerCase();
 					if (displayValues.hasOwnProperty(nodename))
 						obj.style.display = displayValues[nodename];
-					else obj.style.display = "inline";
+					else obj.style.display = 'inline';
 				});
 			};
-			dom.hide = _ => dom.forEach(obj => (obj.style.display = "none"));
-			dom.click = cb => dom.forEach(obj => obj.addEventListener("click", cb));
+			dom.hide = _ => dom.forEach(obj => (obj.style.display = 'none'));
+			dom.click = cb => dom.forEach(obj => obj.addEventListener('click', cb));
 			dom.attr = (name, value) => (dom[0][name] = value);
 
 			return dom;
@@ -465,10 +465,10 @@
 	};
 
 	exports.parseUrl = url => {
-		url = url.split("%20").join(" ");
-		const dir = url.split("/");
+		url = url.split('%20').join(' ');
+		const dir = url.split('/');
 		dir.shift();
-		const q = dir[dir.length - 1].split("?"),
+		const q = dir[dir.length - 1].split('?'),
 			r = {
 				dir: dir,
 				params: {
@@ -476,9 +476,9 @@
 				}
 			};
 		if (q.length > 1) {
-			q[1].split("&").forEach(item => {
-				const obj = item.split("=");
-				r.params[obj[0]] = obj.length > 1 ? obj[1] : "";
+			q[1].split('&').forEach(item => {
+				const obj = item.split('=');
+				r.params[obj[0]] = obj.length > 1 ? obj[1] : '';
 				r.params.length++;
 			});
 			dir[dir.length - 1] = q[0];
@@ -488,24 +488,24 @@
 
 	exports.cleanup = callback => {
 
-		process.on("cleanup", callback);
+		process.on('cleanup', callback);
 
 		// do app specific cleaning before exiting
-		process.on("exit", _ => {
-			process.emit("cleanup");
+		process.on('exit', _ => {
+			process.emit('cleanup');
 		});
 
 		// catch ctrl+c event and exit normally
-		process.on("SIGINT", _ => {
-			process.emit("cleanup");
+		process.on('SIGINT', _ => {
+			process.emit('cleanup');
 			process.exit(2);
 		});
 
 		//catch uncaught exceptions, trace, then exit normally
-		process.on("uncaughtException", e => {
-			print("Uncaught Exception...");
-			error(e.stack);
-			process.emit("cleanup");
+		process.on('uncaughtException', e => {
+			console.error('Uncaught Exception...');
+			console.error(e.stack);
+			process.emit('cleanup');
 			process.exit(99);
 		});
 	};
@@ -575,147 +575,147 @@
 	//US ------------------- UK
 	exports.colorNameToHex = exports.colourNameToHex = c => {
 		const colours = {
-			"aliceblue": "#f0f8ff",
-			"antiquewhite": "#faebd7",
-			"aqua": "#00ffff",
-			"aquamarine": "#7fffd4",
-			"azure": "#f0ffff",
-			"beige": "#f5f5dc",
-			"bisque": "#ffe4c4",
-			"black": "#000000",
-			"blanchedalmond": "#ffebcd",
-			"blue": "#0000ff",
-			"blueviolet": "#8a2be2",
-			"brown": "#a52a2a",
-			"burlywood": "#deb887",
-			"cadetblue": "#5f9ea0",
-			"chartreuse": "#7fff00",
-			"chocolate": "#d2691e",
-			"coral": "#ff7f50",
-			"cornflowerblue": "#6495ed",
-			"cornsilk": "#fff8dc",
-			"crimson": "#dc143c",
-			"cyan": "#00ffff",
-			"darkblue": "#00008b",
-			"darkcyan": "#008b8b",
-			"darkgoldenrod": "#b8860b",
-			"darkgray": "#a9a9a9",
-			"darkgreen": "#006400",
-			"darkkhaki": "#bdb76b",
-			"darkmagenta": "#8b008b",
-			"darkolivegreen": "#556b2f",
-			"darkorange": "#ff8c00",
-			"darkorchid": "#9932cc",
-			"darkred": "#8b0000",
-			"darksalmon": "#e9967a",
-			"darkseagreen": "#8fbc8f",
-			"darkslateblue": "#483d8b",
-			"darkslategray": "#2f4f4f",
-			"darkturquoise": "#00ced1",
-			"darkviolet": "#9400d3",
-			"deeppink": "#ff1493",
-			"deepskyblue": "#00bfff",
-			"dimgray": "#696969",
-			"dodgerblue": "#1e90ff",
-			"firebrick": "#b22222",
-			"floralwhite": "#fffaf0",
-			"forestgreen": "#228b22",
-			"fuchsia": "#ff00ff",
-			"gainsboro": "#dcdcdc",
-			"ghostwhite": "#f8f8ff",
-			"gold": "#ffd700",
-			"goldenrod": "#daa520",
-			"gray": "#808080",
-			"green": "#008000",
-			"greenyellow": "#adff2f",
-			"honeydew": "#f0fff0",
-			"hotpink": "#ff69b4",
-			"indianred ": "#cd5c5c",
-			"indigo": "#4b0082",
-			"ivory": "#fffff0",
-			"khaki": "#f0e68c",
-			"lavender": "#e6e6fa",
-			"lavenderblush": "#fff0f5",
-			"lawngreen": "#7cfc00",
-			"lemonchiffon": "#fffacd",
-			"lightblue": "#add8e6",
-			"lightcoral": "#f08080",
-			"lightcyan": "#e0ffff",
-			"lightgoldenrodyellow": "#fafad2",
-			"lightgrey": "#d3d3d3",
-			"lightgreen": "#90ee90",
-			"lightpink": "#ffb6c1",
-			"lightsalmon": "#ffa07a",
-			"lightseagreen": "#20b2aa",
-			"lightskyblue": "#87cefa",
-			"lightslategray": "#778899",
-			"lightsteelblue": "#b0c4de",
-			"lightyellow": "#ffffe0",
-			"lime": "#00ff00",
-			"limegreen": "#32cd32",
-			"linen": "#faf0e6",
-			"magenta": "#ff00ff",
-			"maroon": "#800000",
-			"mediumaquamarine": "#66cdaa",
-			"mediumblue": "#0000cd",
-			"mediumorchid": "#ba55d3",
-			"mediumpurple": "#9370d8",
-			"mediumseagreen": "#3cb371",
-			"mediumslateblue": "#7b68ee",
-			"mediumspringgreen": "#00fa9a",
-			"mediumturquoise": "#48d1cc",
-			"mediumvioletred": "#c71585",
-			"midnightblue": "#191970",
-			"mintcream": "#f5fffa",
-			"mistyrose": "#ffe4e1",
-			"moccasin": "#ffe4b5",
-			"navajowhite": "#ffdead",
-			"navy": "#000080",
-			"oldlace": "#fdf5e6",
-			"olive": "#808000",
-			"olivedrab": "#6b8e23",
-			"orange": "#ffa500",
-			"orangered": "#ff4500",
-			"orchid": "#da70d6",
-			"palegoldenrod": "#eee8aa",
-			"palegreen": "#98fb98",
-			"paleturquoise": "#afeeee",
-			"palevioletred": "#d87093",
-			"papayawhip": "#ffefd5",
-			"peachpuff": "#ffdab9",
-			"peru": "#cd853f",
-			"pink": "#ffc0cb",
-			"plum": "#dda0dd",
-			"powderblue": "#b0e0e6",
-			"purple": "#800080",
-			"rebeccapurple": "#663399",
-			"red": "#ff0000",
-			"rosybrown": "#bc8f8f",
-			"royalblue": "#4169e1",
-			"saddlebrown": "#8b4513",
-			"salmon": "#fa8072",
-			"sandybrown": "#f4a460",
-			"seagreen": "#2e8b57",
-			"seashell": "#fff5ee",
-			"sienna": "#a0522d",
-			"silver": "#c0c0c0",
-			"skyblue": "#87ceeb",
-			"slateblue": "#6a5acd",
-			"slategray": "#708090",
-			"snow": "#fffafa",
-			"springgreen": "#00ff7f",
-			"steelblue": "#4682b4",
-			"tan": "#d2b48c",
-			"teal": "#008080",
-			"thistle": "#d8bfd8",
-			"tomato": "#ff6347",
-			"turquoise": "#40e0d0",
-			"violet": "#ee82ee",
-			"wheat": "#f5deb3",
-			"white": "#ffffff",
-			"whitesmoke": "#f5f5f5",
-			"yellow": "#ffff00",
-			"yellowgreen": "#9acd32"
+			'aliceblue': '#f0f8ff',
+			'antiquewhite': '#faebd7',
+			'aqua': '#00ffff',
+			'aquamarine': '#7fffd4',
+			'azure': '#f0ffff',
+			'beige': '#f5f5dc',
+			'bisque': '#ffe4c4',
+			'black': '#000000',
+			'blanchedalmond': '#ffebcd',
+			'blue': '#0000ff',
+			'blueviolet': '#8a2be2',
+			'brown': '#a52a2a',
+			'burlywood': '#deb887',
+			'cadetblue': '#5f9ea0',
+			'chartreuse': '#7fff00',
+			'chocolate': '#d2691e',
+			'coral': '#ff7f50',
+			'cornflowerblue': '#6495ed',
+			'cornsilk': '#fff8dc',
+			'crimson': '#dc143c',
+			'cyan': '#00ffff',
+			'darkblue': '#00008b',
+			'darkcyan': '#008b8b',
+			'darkgoldenrod': '#b8860b',
+			'darkgray': '#a9a9a9',
+			'darkgreen': '#006400',
+			'darkkhaki': '#bdb76b',
+			'darkmagenta': '#8b008b',
+			'darkolivegreen': '#556b2f',
+			'darkorange': '#ff8c00',
+			'darkorchid': '#9932cc',
+			'darkred': '#8b0000',
+			'darksalmon': '#e9967a',
+			'darkseagreen': '#8fbc8f',
+			'darkslateblue': '#483d8b',
+			'darkslategray': '#2f4f4f',
+			'darkturquoise': '#00ced1',
+			'darkviolet': '#9400d3',
+			'deeppink': '#ff1493',
+			'deepskyblue': '#00bfff',
+			'dimgray': '#696969',
+			'dodgerblue': '#1e90ff',
+			'firebrick': '#b22222',
+			'floralwhite': '#fffaf0',
+			'forestgreen': '#228b22',
+			'fuchsia': '#ff00ff',
+			'gainsboro': '#dcdcdc',
+			'ghostwhite': '#f8f8ff',
+			'gold': '#ffd700',
+			'goldenrod': '#daa520',
+			'gray': '#808080',
+			'green': '#008000',
+			'greenyellow': '#adff2f',
+			'honeydew': '#f0fff0',
+			'hotpink': '#ff69b4',
+			'indianred ': '#cd5c5c',
+			'indigo': '#4b0082',
+			'ivory': '#fffff0',
+			'khaki': '#f0e68c',
+			'lavender': '#e6e6fa',
+			'lavenderblush': '#fff0f5',
+			'lawngreen': '#7cfc00',
+			'lemonchiffon': '#fffacd',
+			'lightblue': '#add8e6',
+			'lightcoral': '#f08080',
+			'lightcyan': '#e0ffff',
+			'lightgoldenrodyellow': '#fafad2',
+			'lightgrey': '#d3d3d3',
+			'lightgreen': '#90ee90',
+			'lightpink': '#ffb6c1',
+			'lightsalmon': '#ffa07a',
+			'lightseagreen': '#20b2aa',
+			'lightskyblue': '#87cefa',
+			'lightslategray': '#778899',
+			'lightsteelblue': '#b0c4de',
+			'lightyellow': '#ffffe0',
+			'lime': '#00ff00',
+			'limegreen': '#32cd32',
+			'linen': '#faf0e6',
+			'magenta': '#ff00ff',
+			'maroon': '#800000',
+			'mediumaquamarine': '#66cdaa',
+			'mediumblue': '#0000cd',
+			'mediumorchid': '#ba55d3',
+			'mediumpurple': '#9370d8',
+			'mediumseagreen': '#3cb371',
+			'mediumslateblue': '#7b68ee',
+			'mediumspringgreen': '#00fa9a',
+			'mediumturquoise': '#48d1cc',
+			'mediumvioletred': '#c71585',
+			'midnightblue': '#191970',
+			'mintcream': '#f5fffa',
+			'mistyrose': '#ffe4e1',
+			'moccasin': '#ffe4b5',
+			'navajowhite': '#ffdead',
+			'navy': '#000080',
+			'oldlace': '#fdf5e6',
+			'olive': '#808000',
+			'olivedrab': '#6b8e23',
+			'orange': '#ffa500',
+			'orangered': '#ff4500',
+			'orchid': '#da70d6',
+			'palegoldenrod': '#eee8aa',
+			'palegreen': '#98fb98',
+			'paleturquoise': '#afeeee',
+			'palevioletred': '#d87093',
+			'papayawhip': '#ffefd5',
+			'peachpuff': '#ffdab9',
+			'peru': '#cd853f',
+			'pink': '#ffc0cb',
+			'plum': '#dda0dd',
+			'powderblue': '#b0e0e6',
+			'purple': '#800080',
+			'rebeccapurple': '#663399',
+			'red': '#ff0000',
+			'rosybrown': '#bc8f8f',
+			'royalblue': '#4169e1',
+			'saddlebrown': '#8b4513',
+			'salmon': '#fa8072',
+			'sandybrown': '#f4a460',
+			'seagreen': '#2e8b57',
+			'seashell': '#fff5ee',
+			'sienna': '#a0522d',
+			'silver': '#c0c0c0',
+			'skyblue': '#87ceeb',
+			'slateblue': '#6a5acd',
+			'slategray': '#708090',
+			'snow': '#fffafa',
+			'springgreen': '#00ff7f',
+			'steelblue': '#4682b4',
+			'tan': '#d2b48c',
+			'teal': '#008080',
+			'thistle': '#d8bfd8',
+			'tomato': '#ff6347',
+			'turquoise': '#40e0d0',
+			'violet': '#ee82ee',
+			'wheat': '#f5deb3',
+			'white': '#ffffff',
+			'whitesmoke': '#f5f5f5',
+			'yellow': '#ffff00',
+			'yellowgreen': '#9acd32'
 		};
 
 		if (typeof colours[c.toLowerCase()] != 'undefined')
@@ -726,21 +726,21 @@
 
 	exports.sum = arr => {
 		if (!Array.isArray(arr))
-			throw "arr() is only compatible with arrays";
+			throw 'arr() is only compatible with arrays';
 		else
 			return arr.reduce((t, o) => t + o, 0);
 	};
 
 	exports.mean = arr => {
 		if (!Array.isArray(arr))
-			throw "mean() is only compatible with arrays";
+			throw 'mean() is only compatible with arrays';
 		else
 			return sum(arr) / arr.length;
 	};
 
 	exports.stddev = arr => {
 		if (!Array.isArray(arr))
-			throw "stddev() is only compatible with arrays";
+			throw 'stddev() is only compatible with arrays';
 		else {
 			const mu = mean(arr);
 			return this.sum(arr.map(v => this.pow(v - mu, 2))) / arr.length;
@@ -763,4 +763,4 @@
 		'&quot;': '"'
 	})[tag] || tag);
 
-})(typeof exports == "undefined" ? typeof window == "undefined" ? self : window : exports);
+})(typeof exports == 'undefined' ? typeof window == 'undefined' ? self : window : exports);
