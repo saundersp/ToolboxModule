73e83f73491b1449dcd2a86e9af7d7ffaeae4929
saundersp <pierre.saundersgb@gmail.com>
2024-07-17 20:55:46 +0200

    sommaire_updater.py : PEP8 compliant

--- sommaire_updater.py
+++ sommaire_updater.py
@@ -1,19 +1,19 @@
-import os
 from re import sub
 
-def getHeaders():
-	lines = []
-	with open('README.md', "r") as readme:
+def getHeaders() -> None:
+	lines: list[str] = []
+	with open('README.md', 'r') as readme:
 		for line in readme.readlines():
-			if '### ' in line and not '####' in line and not '#####' in line:
+			if '### ' in line and '####' not in line and '#####' not in line:
 				lines.append(line)
 
 	with open('TEMP.md', 'w') as out:
 		for fnc in lines:
 			trunc = fnc[fnc.index(' ') + 1:]
 			header = trunc[:trunc.index('(')].rstrip()
-			link = sub('\(|\)|,|\.|\[|\]|\||&', '', trunc.replace(' ', '-'))[:-1].lower()
-			out.write(f"- [{header}](#{link})\n")
+			link = sub('(|)|,|.|[|]|||&', '', trunc.replace(' ', '-'))[:-1].lower()
+			out.write(f'- [{header}](#{link})\n')
 
 if __name__ == '__main__':
 	getHeaders()
+
